d25856a18d4d447fdd31e31f3f271b039f3d5304
H. C. Kruse <hannes@octofox.de>
2023-06-07 10:49:12 +0200

    bug: Fix SCU display for german number formats

--- VehicleHardpoint/VehicleHardpoint.lua
+++ VehicleHardpoint/VehicleHardpoint.lua
@@ -592,7 +592,15 @@ function methodtable.makeSubtitle( self, item )
     end
 
     -- Show SCU in subtitle
-    if item.scu ~= nil and type( item.scu ) == 'number' then
+    if item.scu ~= nil then
+        -- Fix for german number format
+        if string.find( item.scu, ',', 1, true ) then
+            local success, scu = pcall( tonumber, string.gsub( item.scu, ',', '.' ), 10 )
+            if success then
+                item.scu = scu
+            end
+        end
+
         if item.type == translate( 'CargoGrid' ) then
             subtitle = item.scu .. ' SCU' or 'N/A'
         elseif item.type == translate( 'PersonalStorage' ) then
