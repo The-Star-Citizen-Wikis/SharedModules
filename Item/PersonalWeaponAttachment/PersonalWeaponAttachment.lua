238124de39e36bfb3df4dc4dcadba5828673746c
alistair3149 <alistair31494322@gmail.com>
2023-07-13 20:00:39 -0400

    feat(Item/PersonalWeaponAttachment): WIP setup files

---
+++ Item/PersonalWeaponAttachment/PersonalWeaponAttachment.lua
@@ -0,0 +1,82 @@
+require( 'strict' )
+
+local Template = {}
+
+local TNT = require( 'Module:Translate' ):new()
+local smwCommon = require( 'Module:Common/SMW' )
+local data = mw.loadJsonData( 'Module:Item/PersonalWeaponAttachment/data.json' )
+local config = mw.loadJsonData( 'Module:Item/config.json' )
+
+
+--- Wrapper function for Module:Translate.translate
+---
+--- @param key string The translation key
+--- @param addSuffix boolean Adds a language suffix if config.smw_multilingual_text is true
+--- @return string If the key was not found in the .tab page, the key is returned
+local function translate( key, addSuffix, ... )
+    return TNT:translate( 'Module:Item/PersonalWeaponAttachment/i18n.json', config, key, addSuffix, {...} )
+end
+
+
+--- Adds the properties valid for this item to the SMW Set object
+---
+--- @param smwSetObject table
+function PersonalWeaponAttachment.addSmwProperties( apiData, frameArgs, smwSetObject )
+    smwCommon.addSmwProperties(
+        apiData,
+        frameArgs,
+        smwSetObject,
+        translate,
+        config,
+        data,
+        'PersonalWeaponAttachment'
+    )
+end
+
+
+--- Adds all SMW parameters set by this Module to the ASK object
+---
+--- @param smwAskObject table
+--- @return void
+function PersonalWeaponAttachment.addSmwAskProperties( smwAskObject )
+    smwCommon.addSmwQueryParams(
+        smwAskObject,
+        translate,
+        config,
+        data
+    )
+end
+
+
+--- Adds entries to the infobox
+---
+--- @param infobox table The Module:InfoboxNeue instance
+--- @param smwData table Data from Semantic MediaWiki
+--- @return void
+function PersonalWeaponAttachment.addInfoboxData( infobox, smwData )
+
+end
+
+
+--- Add categories that are set on the page.
+--- The categories table should only contain category names, no MW Links, i.e. 'Foo' instead of '[[Category:Foo]]'
+---
+--- @param categories table The categories table
+--- @param frameArgs table Frame arguments from Module:Arguments
+--- @param smwData table Data from Semantic MediaWiki
+--- @return void
+function PersonalWeaponAttachment.addCategories( categories, frameArgs, smwData )
+
+end
+
+--- Return the short description for this object
+---
+--- @param frameArgs table Frame arguments from Module:Arguments
+--- @param smwData table Data from Semantic MediaWiki
+--- @return string|nil
+function PersonalWeaponAttachment.getShortDescription( frameArgs, smwData )
+	
+end
+
+
+return PersonalWeaponAttachment
