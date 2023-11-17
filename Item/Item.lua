fb8326677f257d29f84a238d8a435128c865495d
Astrid <hello-from-github@robertsspaceindustri.es>
2023-11-17 18:05:29 +0100

    feat(Item): add size & grade categories if applicable (#1)
    * feat(Item): add size & grade categories if applicable
    
    * Add changes as discussed
    
    * Update QED name (en)
    
    * Fix line position
    
    * QED type capitalization
    
    * Fix lowercaseWords(), wasn't detecting uppercase words
    
    * nit: Fix formatting
    
    ---------
    
    Co-authored-by: H. C. Kruse <hannes@octofox.de>

--- Item/Item.lua
+++ Item/Item.lua
@@ -410,10 +410,42 @@ function methodtable.setCategories( self )
 		return
 	end
 
+	--- Lowers all words unless the word is fully capitalized (e.g.: EMP Generators -> EMP generators)
+	local function lowercaseWords( input )
+	    local result = input:gsub( '(%S+)' , function( word )
+    	    if string.upper( word ) == word then
+        	    return word
+    	    end
+
+			return string.lower( word )
+	    end)
+	    return result
+	end
+	
+	local function startsWith( str, start )
+		return str:sub( 1, #start ) == start
+	end
+
 	--- Only set category if category_type value exists
 	if self.smwData[ translate( 'SMW_Type' ) ] ~= nil then
 		local typeCategory = translate( 'category_' .. string.lower( self.smwData[ translate( 'SMW_Type' ) ] ) )
-		if typeCategory ~= nil and typeCategory ~= 'category_' .. string.lower( self.smwData[ translate( 'SMW_Type' ) ] ) then table.insert( self.categories, typeCategory ) end
+
+		if typeCategory ~= nil and typeCategory ~= 'category_' .. string.lower( self.smwData[ translate( 'SMW_Type' ) ] ) then
+			table.insert( self.categories, typeCategory ) 
+			
+			local categoryTypeSuffix = translate( 'category_' .. string.lower( self.smwData[ translate( 'SMW_Type' ) ] ) )
+			if startsWith( lang:getCode(), 'en' ) then categoryTypeSuffix = lowercaseWords( categoryTypeSuffix ) end
+
+			if self.smwData[ translate( 'SMW_Size' ) ] ~= nil then
+				local sizeCategory = translate( 'SMW_Size' ) .. ' ' .. self.smwData[ translate( 'SMW_Size' ) ] .. ' ' .. categoryTypeSuffix
+				table.insert( self.categories, sizeCategory )
+			end
+
+			if self.smwData[ translate( 'SMW_Grade' ) ] ~= nil then
+				local gradeCategory = translate( 'SMW_Grade' ) .. ' ' .. self.smwData[ translate( 'SMW_Grade' ) ] .. ' ' .. categoryTypeSuffix
+				table.insert( self.categories, gradeCategory )
+			end
+		end
 	end
 
 	if self.smwData[ translate( 'SMW_Manufacturer' ) ] ~= nil then
