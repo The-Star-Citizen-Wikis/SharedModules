971f16692857a7c70dcb4932c6621863741e3d10
Astrid <github@astrid.exposed>
2023-11-18 06:49:05 +0100

    Update

--- System/System.lua
+++ System/System.lua
@@ -6,8 +6,8 @@ local TNT = require( 'Module:Translate' ):new()
 local config = mw.loadJsonData( 'Module:System/config.json' )
 
 local lang
-if config.module_lang then
-	lang = mw.getLanguage( config.module_lang )
+if config[ 'module_lang' ] then
+	lang = mw.getLanguage( config[ 'module_lang' ] )
 else
 	lang = mw.getContentLanguage()
 end
@@ -19,6 +19,11 @@ local langCode = lang:getCode()
 -- @return string If the key was not found in the .tab page, the key is returned
 local function t( key, addSuffix, ... )
 	return TNT:translate( 'Module:System/i18n.json', config, key, addSuffix, {...} ) or key
+end
+
+--- Remove parentheses and their content
+function removeParentheses( inputString )
+    return string.gsub( inputString, '%b()', '' )
 end
 
 --- Alternative for doing table[key][key], this returns nil instead of an error if it doesn't exist
@@ -56,13 +61,13 @@ end
 -- @param zero any Value to return if zero matches
 local function filter( array, key, value, zero )
 	local matches = {}
-	
-	for _, item in ipairs( array ) do
-		if item[ key ] == value then
-			table.insert( matches, item )
+	if array then
+		for _, item in ipairs( array ) do
+			if item[ key ] == value then
+				table.insert( matches, item )
+			end
 		end
 	end
-	
 	if zero and #matches == 0 then
 		return zero
 	else
@@ -75,11 +80,9 @@ end
 -- @param sep string Seperator
 local function split( str, sep )
 	local matches = {}
-	
 	for str in string.gmatch( str, '([^' .. sep .. ']+)' ) do
 		table.insert( matches, str )
 	end
-	
 	return matches
 end
 
@@ -94,7 +97,6 @@ end
 -- @param categories table Plain text categories in array
 local function convertCategories( categories )
 	local mapped = {}
-
 	for _, category in pairs( categories ) do
 		if category ~= nil then
 			if string.sub( category, 1, 2 ) ~= '[[' then
@@ -104,7 +106,6 @@ local function convertCategories( categories )
 			table.insert( mapped, category )
 		end
 	end
-
 	return table.concat( mapped )
 end
 
@@ -113,194 +114,283 @@ function System.main( frame )
 	local args = frame:getParent().args
 	local infobox = Infobox:new( { placeholderImage = config[ 'placeholder_image' ] } )
 	
-	local name = args[ 'name' ]
-	local code = args[ 'code' ]
-	local system = Starmap.findStructure( 'system', code or name ) or {}
+	--- The idea of this mega table is to get all data, then set it in the infobox, better organization.
+	local mega = {
+		[ 'image' ] = nil,
+		[ 'name' ] = nil,
+		[ '#name' ] = nil,
+		[ 'code' ] = nil,
+		[ 'system' ] = {},
+		[ 'type' ] = nil,
+		[ '#type' ] = nil,
+		[ 'size' ] = nil,
+		[ '#size' ] = nil,
+		[ 'status' ] = nil,
+		[ '#status' ] = nil,
+		[ 'system_objects' ] = {},
+		[ 'star_types' ] = {},
+		[ '#star_types' ] = nil,
+		[ 'affiliation' ] = {},
+		[ '#affiliation' ] = nil,
+		[ '#population' ] = nil,
+		[ 'planet_count' ] = nil,
+		[ 'satellite_count' ] = nil,
+		[ 'asteroid_belt_count' ] = nil,
+		[ 'asteroid_field_count' ] = nil,
+		[ 'anomaly_count' ] = nil,
+		[ 'station_count' ] = nil,
+		[ 'jumppoint_count' ] = nil,
+		[ 'blackhole_count' ] = nil,
+		[ 'poi_count' ] = nil,
+		[ 'sensor_danger' ] = nil,
+		[ 'sensor_economy' ] = nil,
+		[ 'sensor_population' ] = nil,
+		[ 'discovered_in' ] = nil,
+		[ 'discovered_by' ] = nil,
+		[ 'historical_names' ] = {},
+		[ '#historical_names' ] = nil,
+		[ 'starmap_link' ] = nil,
+		[ 'starmap_id' ] = nil,
+		[ 'categories' ] = {},
+	}
 	
-	if (name == nil or name == '') and system[ 'name' ] then name = system[ 'name' ] end
-	if (code == nil or code == '') and system[ 'code' ] then code = system[ 'code' ] end
+	table.insert( mega[ 'categories' ], 'Systems' ) -- Default category for systems
 	
-	if name == nil then
+	mega[ 'image' ] = args[ 'image' ]
+	mega[ 'name' ] = args[ 'name' ]
+	mega[ 'code' ] = args[ 'code' ]
+	if mega[ 'name' ] == nil and mega[ 'code' ] == nil then
 		return infobox:renderInfobox( infobox:renderMessage( { 
-			title = t( 'error_no_data_title' ),
-			desc = t( 'error_no_data_desc' )
+			title = t( 'error_title' ),
+			desc = t( 'error_invalid_args_desc' )
 		} ) )
 	end
 	
-	local type = args[ 'type' ] or system[ 'type' ]
-	local status = args[ 'status' ] or system[ 'status' ]
-	local translatedType = nil
-	local translatedStatus = nil
-	if type then translatedType = t( 'val_type_' .. string.lower( type ) ) end
-	if status then translatedStatus = t( 'val_status_' .. string.lower( status ) ) end
-	
-	local image = args[ 'image' ]
-	local systemObjects = Starmap.systemObjects( code or name ) or {}
-	local planets = args[ 'planets' ] or #filter( systemObjects, 'type', 'PLANET', '' )
-	local satellites = args[ 'satellites' ] or #filter( systemObjects, 'type', 'SATELLITE', '' )
-	local asteroidBelts = args[ 'asteroidbelts' ] or #filter( systemObjects, 'type', 'ASTEROID_BELT', '' )
-	local asteroidFields = args[ 'asteroidfields' ] or #filter( systemObjects, 'type', 'ASTEROID_FIELDS', '' )
-	local anomalies = args[ 'anomalies' ] or #filter( systemObjects, 'type', 'ANOMALY', '' )
-	local stations = args[ 'stations' ] or #filter( systemObjects, 'type', 'MANMADE', '' )
-	local jumppoints = args[ 'jumppoints' ] or #filter( systemObjects, 'type', 'JUMPPOINT', '' )
-	local blackholes = args[ 'blackholes' ] or #filter( systemObjects, 'type', 'BLACKHOLE', '' )
-	local pois = args[ 'pois' ] or #filter( systemObjects, 'type', 'POI', '' )
-	local starTypes = args[ 'startypes' ]
-	local starmapLink = args[ 'starmap' ] or inlineIf( code, Starmap.link( code ), nil  )
-	local discoveredIn = args[ 'discoveredin' ]
-	local discoveredBy = args[ 'discoveredby' ]
-	local sensorDanger = inlineIf( args[ 'danger' ], tonumber( args[ 'danger' ] ), tonumber( e( system, 'aggregated_danger' ) or 0 ) )
-	local sensorEconomy = inlineIf( args[ 'economy' ], tonumber( args[ 'economy' ] ), tonumber( e( system, 'aggregated_economy' ) or 0 ) )
-	local sensorPopulation = inlineIf( args[ 'population' ], tonumber( args[ 'population' ] ), tonumber( e( system, 'aggregated_population' ) or 0 ) )
-	local historicalNames = args[ 'historicalnames' ]
-	local population = args[ 'population' ]
-	local affiliation = args[ 'affiliation' ] or e( system, 'affiliation', 1, 'name' )
-	local size = inlineIf( system[ 'aggregated_size' ], tonumber( system[ 'aggregated_size' ] ), nil ) -- Argument version is checked for later
-	local affiliationLink = nil
-	if affiliation then affiliationLink = inlineIf( startsWith( affiliation, '[[' ), '', '[[' ) .. affiliation .. inlineIf( endsWith( affiliation, ']]' ), '', ']]' ) end
-	
-	local starTypeArray = {}
-	if starTypes == nil or starTypes == '' then
-		local stars = filter( systemObjects, 'type', 'STAR' )
-		
-		for _, star in ipairs( stars ) do
-			if star[ 'subtype' ] then
-				table.insert( starTypeArray, config[ 'subtype_rename' ][ star[ 'subtype' ][ 'name' ] ] or star[ 'subtype' ][ 'name' ] )
-			end
-		end
-		
-		starTypes = table.concat( starTypeArray, ', ' )
+	mega[ 'system' ] = Starmap.findStructure( 'system', mega[ 'code' ] or mega[ 'name' ] ) or {}
+	if mega[ 'system' ] ~= nil then
+		if mega[ 'name' ] == nil then mega[ 'name' ] = removeParentheses( mega[ 'system' ][ 'name' ] ) end
+		mega[ 'code' ] = mega[ 'system' ][ 'code' ]
 	end
 	
-	infobox:renderImage( image )
+	mega[ '#name' ] = mega[ 'name' ]
+	--- Add ' system' to end of `#name`
+	if endsWith( mega[ '#name' ], ' system' ) == false then
+		mega[ '#name' ] = mega[ '#name' ] .. ' system'
+	end
 	
-	if endsWith( name, ' system' ) == false then name = name .. ' system' end
+	mega[ 'type' ] = args[ 'type' ] or mega[ 'system' ][ 'type' ]
+	if mega[ 'type' ] ~= nil then
+		mega[ '#type' ] = t( 'val_type_' .. string.lower( mega[ 'type' ] ) )
+		table.insert( mega[ 'categories' ], mega[ '#type' ] .. ' Systems' )
+	end
 	
+	mega[ 'size' ] = args[ 'size' ] or mega[ 'system' ][ 'aggregated_size' ]
+	if mega[ 'size' ] ~= nil and tonumber( mega[ 'size' ] ) then
+		mega[ 'size' ] = tonumber( mega[ 'size' ] )
+		mega[ '#size' ] = tostring( mega[ 'size' ] ) .. ' AU'
+	else mega[ '#size' ] = '? AU' end
+	
+	mega[ 'status' ] = args[ 'status' ] or mega[ 'system' ][ 'status' ]
+	if mega[ 'status' ] ~= nil then mega[ '#status' ] = t( 'val_status_' .. string.lower( mega[ 'status' ] ) ) end
+	
+	mega[ 'system_objects' ] = Starmap.systemObjects( mega[ 'code' ] or mega[ 'name' ] )
+	
+	mega[ 'star_types' ] = args[ 'startypes' ]
+	if mega[ 'star_types' ] then
+		mega[ 'star_types' ] = split( mega[ 'star_types' ], ', ' )
+	elseif mega[ 'system_objects' ] then
+		mega[ 'star_types' ] = {}
+		for _, star in ipairs( filter( mega[ 'system_objects' ], 'type', 'STAR' ) ) do
+			if star[ 'subtype' ] then
+				table.insert( mega[ 'star_types' ], config[ 'subtype_rename' ][ star[ 'subtype' ][ 'name' ] ] or star[ 'subtype' ][ 'name' ] )
+			end
+		end
+	else
+		mega[ 'star_types' ] = {} -- Revert back to default
+	end
+	mega[ '#star_types' ] = table.concat( mega[ 'star_types' ], ', ' )
+	
+	if args[ 'affiliation' ] then
+		mega[ 'affiliation' ] = split( args[ 'affiliation' ], ', ' )
+	elseif e( mega, 'system', 'affiliation' ) ~= nil then
+		mega[ 'affiliation' ] = {}
+		for _, empire in ipairs( mega[ 'system' ][ 'affiliation' ] ) do
+			table.insert( mega[ 'affiliation' ], empire[ 'name' ] )
+		end
+	end
+	if mega[ 'affiliation' ][ 1 ] then table.insert( mega[ 'categories' ], mega[ 'affiliation' ][ 1 ] .. ' Systems' ) end
+	mega[ '#affiliation' ] = {}
+	for _, name in ipairs( mega[ 'affiliation' ] ) do
+		table.insert( mega[ '#affiliation' ], string.format( '[[%s]]', name ) )
+	end
+	mega[ '#affiliation' ] = table.concat( mega[ '#affiliation' ], ', ' )
+	
+	mega[ '#population' ] = args[ 'population' ]
+	
+	mega[ 'planet_count' ] = args[ 'planets' ] or #filter( mega[ 'system_objects' ], 'type', 'PLANET', nil )
+	mega[ 'satellite_count' ] = args[ 'satellites' ] or #filter( mega[ 'system_objects' ], 'type', 'SATELLITE', nil )
+	mega[ 'asteroid_belt_count' ] = args[ 'asteroidbelts' ] or #filter( mega[ 'system_objects' ], 'type', 'ASTEROID_BELT', nil )
+	mega[ 'asteroid_fields_count' ] = args[ 'asteroidfields' ] or #filter( mega[ 'system_objects' ], 'type', 'ASTEROID_FIELDS', nil )
+	mega[ 'anomaly_count' ] = args[ 'anomalies' ] or #filter( mega[ 'system_objects' ], 'type', 'ANOMALY', nil )
+	mega[ 'station_count' ] = args[ 'stations' ] or #filter( mega[ 'system_objects' ], 'type', 'MANMADE', nil )
+	mega[ 'jumppoint_count' ] = args[ 'jumppoints' ] or #filter( mega[ 'system_objects' ], 'type', 'JUMPPOINT', nil )
+	mega[ 'blackholes_count' ] = args[ 'blackholes' ] or #filter( mega[ 'system_objects' ], 'type', 'BLACKHOLE', nil )
+	mega[ 'poi_count' ] = args[ 'pois' ] or #filter( mega[ 'system_objects' ], 'type', 'POI', nil )
+	
+	mega[ 'sensor_danger' ] = args[ 'sensordanger' ] or e( mega[ 'system' ][ 'aggregated_danger' ] )
+	if mega[ 'sensor_danger' ] ~= nil and mega[ 'sensor_danger' ] ~= 0 then
+		mega[ 'sensor_danger' ] = tonumber( mega[ 'sensor_danger' ] )
+		mega[ '#sensor_danger' ] = tostring( mega[ 'sensor_danger' ] ) .. '/10'
+	else mega[ 'sensor_danger' ] = nil end
+	
+	mega[ 'sensor_economy' ] = args[ 'sensoreconomy' ] or e( mega[ 'system' ][ 'aggregated_economy' ] )
+	if mega[ 'sensor_economy' ] ~= nil and mega[ 'sensor_economy' ] ~= 0 then
+		mega[ 'sensor_economy' ] = tonumber( mega[ 'sensor_economy' ] )
+		mega[ '#sensor_economy' ] = tostring( mega[ 'sensor_economy' ] ) .. '/10'
+	else mega[ 'sensor_danger' ] = nil end
+	
+	mega[ 'sensor_population' ] = args[ 'sensorpopulation' ] or e( mega[ 'system' ][ 'aggregated_population' ] )
+	if mega[ 'sensor_population' ] ~= nil and mega[ 'sensor_population' ] ~= 0 then
+		mega[ 'sensor_population' ] = tonumber( mega[ 'sensor_population' ] )
+		mega[ '#sensor_population' ] = tostring( mega[ 'sensor_population' ] ) .. '/10'
+	else mega[ 'sensor_population' ] = nil end
+	
+	mega[ 'discovered_in' ] = args[ 'discoveredin' ]
+	mega[ 'discovered_by' ] = args[ 'discoveredby' ]
+	mega[ 'historical_names' ] = args[ 'historicalnames' ]
+	if mega[ 'historical_names' ] then
+		mega[ 'historical_names' ] = split( mega[ 'historical_names' ], ', ' )
+		mega[ '#historical_names' ] = table.concat( mega[ 'historical_names' ], ', ' )
+	else
+		mega[ 'historical_names' ] = {} -- Revert back to default
+	end
+	
+	mega[ 'starmap_link' ] = args[ 'starmap']
+	if mega[ 'starmap_link' ] == nil and mega[ 'code' ] then mega[ 'starmap_link' ] = Starmap.link( mega[ 'code' ] ) end
+	mega[ 'starmap_id' ] = e( mega, 'system', 'id' )
+	
+	
+	infobox:renderImage( mega[ 'image' ] )
 	infobox:renderHeader( {
-		title = name,
-		subtitle = affiliationLink
+		title = mega[ '#name' ],
+		subtitle = mega[ '#affiliation' ]
 	} )
-
 	infobox:renderSection( {
 		content = {
 			infobox:renderItem( {
 				label = t( 'lbl_type' ),
-				data = translatedType,
+				data = mega[ '#type' ],
 			} ),
 			infobox:renderItem( {
 				label = t( 'lbl_size' ),
-				data = args[ 'size' ] or inlineIf( size, tostring( size ) .. ' AU', '? AU' ),
+				data = mega[ '#size' ],
 			} ),
 			infobox:renderItem( {
 				label = t( 'lbl_status' ),
-				data = translatedStatus,
+				data = mega[ '#status' ],
 			} ),
 			infobox:renderItem( {
-				label = inlineIf( #split( starTypes or '', ',' ) == 1, t( 'lbl_star_type' ), t( 'lbl_star_types' ) ),
-				data = starTypes,
+				label = inlineIf( #mega[ 'star_types' ] == 1, t( 'lbl_star_type' ), t( 'lbl_star_types' ) ),
+				data = mega[ '#star_types' ],
 			} ),
 			infobox:renderItem( {
 				label = t( 'lbl_population' ),
-				data = population,
+				data = mega[ '#population' ],
 			} ),
 		},
 		col = 2
 	} )
-
 	infobox:renderSection( {
 		title = t( 'lbl_astronomical_objects' ),
 		content = {
 			infobox:renderItem( {
 				label = t( 'lbl_planets' ),
-				data = inlineIf( planets == 0, nil, tostring( planets ) )
+				data = inlineIf( (mega[ 'planet_count' ] or nil) ~= 0, mega[ 'planet_count' ] or nil, nil )
 			} ),
 			infobox:renderItem( {
 				label = t( 'lbl_satellites' ),
-				data = inlineIf( satellites == 0, nil, tostring( satellites ) )
+				data = inlineIf( (mega[ 'satellite_count' ] or nil) ~= 0, mega[ 'satellite_count' ] or nil, nil )
 			} ),
 			infobox:renderItem( {
 				label = t( 'lbl_asteroid_belts' ),
-				data = inlineIf( asteroidFields == 0, nil, tostring( asteroidFields ) )
+				data = inlineIf( (mega[ 'asteroid_belt_count' ] or nil) ~= 0, mega[ 'asteroid_belt_count' ] or nil, nil )
 			} ),
 			infobox:renderItem( {
 				label = t( 'lbl_asteroid_fields' ),
-				data = inlineIf( asteroidFields == 0, nil, tostring( asteroidFields ) )
+				data = inlineIf( (mega[ 'asteroid_field_count' ] or nil) ~= 0, mega[ 'asteroid_field_count' ] or nil, nil )
 			} ),
 			infobox:renderItem( {
 				label = t( 'lbl_anomalies' ),
-				data = inlineIf( anomalies == 0, nil, tostring( anomalies ) )
+				data = inlineIf( (mega[ 'anomaly_count' ] or nil) ~= 0, mega[ 'anomaly_count' ] or nil, nil )
 			} ),
 			infobox:renderItem( {
 				label = t( 'lbl_stations' ),
-				data = inlineIf( stations == 0, nil, tostring( stations ) )
+				data = inlineIf( (mega[ 'station_count' ] or nil) ~= 0, mega[ 'station_count' ] or nil, nil )
 			} ),
 			infobox:renderItem( {
 				label = t( 'lbl_jump_points' ),
-				data = inlineIf( jumppoints == 0, nil, tostring( jumppoints ) )
+				data = inlineIf( (mega[ 'jumppoint_count' ] or nil) ~= 0, mega[ 'jumppoint_count' ] or nil, nil )
 			} ),
 			infobox:renderItem( {
 				label = t( 'lbl_blackholes' ),
-				data = inlineIf( blackholes == 0, nil, tostring( blackholes ) )
+				data = inlineIf( (mega[ 'blackhole_count' ] or nil) ~= 0, mega[ 'blackhole_count' ] or nil, nil )
 			} ),
 			infobox:renderItem( {
 				label = t( 'lbl_pois' ),
-				data = inlineIf( pois == 0, nil, tostring( pois ) )
+				data = inlineIf( (mega[ 'poi_count' ] or nil) ~= 0, mega[ 'poi_count' ] or nil, nil )
 			} )
 		},
 		col = 3
 	} )
-
-	if sensorDanger ~= 0 and sensorEconomy ~= 0 and sensorPopulation ~= 0 then
+	if mega[ 'sensor_danger' ] ~= 0 and mega[ 'sensor_economy' ] ~= 0 and mega[ 'sensor_population' ] ~= 0 then
 		infobox:renderSection( {
 			title = t( 'lbl_sensors' ),
 			content = {
 				infobox:renderItem( {
 					label = t( 'lbl_sensor_danger' ),
-					data = inlineIf( sensorDanger, tostring( sensorDanger ) .. '/10', nil ) 
+					data = mega[ '#sensor_danger' ]
 				} ),
 				infobox:renderItem( {
 					label = t( 'lbl_sensor_economy' ),
-					data = inlineIf( sensorEconomy, tostring( sensorEconomy ) .. '/10', nil ) 
+					data = mega[ '#sensor_economy' ]
 				} ),
 				infobox:renderItem( {
 					label = t( 'lbl_sensor_population' ),
-					data = inlineIf( sensorPopulation, tostring( sensorPopulation ) .. '/10', nil ) 
+					data = mega[ '#sensor_population' ]
 				} )
 			},
 			col = 3
 		} )
 	end
-
 	infobox:renderSection( {
 		title = t( 'lbl_history' ),
 		content = {
 			infobox:renderItem( {
 				label = t( 'lbl_discovered_in' ),
-				data = discoveredIn,
+				data = mega[ 'discovered_in' ],
 			} ),
 			infobox:renderItem( {
 				label = t( 'lbl_discovered_by' ),
-				data = discoveredBy,
+				data = mega[ 'discovered_by' ],
 			} ),
 			infobox:renderItem( {
-				label = inlineIf( #split( historicalNames or '', ',' ) == 1, t( 'lbl_historical_name' ), t( 'lbl_historical_names' ) ),
-				data = historicalNames,
+				label = inlineIf( #mega[ 'historical_names' ] == 1, t( 'lbl_historical_name' ), t( 'lbl_historical_names' ) ),
+				data = mega[ '#historical_names' ],
 			} )
 		},
 		col = 2
 	} )
-
 	infobox:renderFooter( {
 		content = {
 			infobox:renderItem( {
 				label = t( 'lbl_starmap_id' ),
-				data = inlineIf( system[ 'id' ], tostring( system[ 'id' ] ), nil ),
+				data = mega[ 'starmap_id' ],
 				row = true,
 				spacebetween = true
 			} ),
 			infobox:renderItem( {
 				label = t( 'lbl_starmap_code' ),
-				data = code,
+				data = mega[ 'code' ],
 				row = true,
 				spacebetween = true
 			} ),
@@ -315,7 +405,7 @@ function System.main( frame )
 						label = t( 'lbl_official_sites' ),
 						data = infobox:renderLinkButton( {
 							label = t( 'lbl_starmap' ),
-							link = starmapLink
+							link = mega[ 'starmap_link' ]
 						} )
 					} ),
 				},
@@ -324,33 +414,27 @@ function System.main( frame )
 		}
 	} )
 
+
 	mw.smw.set( {
-		[ t( 'lbl_starmap_id' ) ] = inlineIf( system[ 'id' ], tostring( system[ 'id' ] ), nil ),
-		[ t( 'lbl_starmap_code' ) ] = code,
-		[ t( 'lbl_system_type' ) ] = translatedType,
-		[ t( 'lbl_system_size' ) ] = size,
-		[ t( 'lbl_system_status' ) ] = translatedStatus,
-		[ t( 'lbl_star_type' ) ] = inlineIf( #starTypeArray > 0, starTypeArray, starTypes )
+		[ t( 'lbl_starmap_id' ) ] = mega[ 'starmap_id' ],
+		[ t( 'lbl_starmap_code' ) ] = mega[ 'code' ],
+		[ t( 'lbl_system_type' ) ] = mega[ 'type' ],
+		[ t( 'lbl_system_size' ) ] = mega[ 'size' ],
+		[ t( 'lbl_system_status' ) ] = mega[ 'status' ],
+		[ t( 'lbl_star_type' ) ] = mega[ 'star_types' ]
 	} )
 
 	frame:callParserFunction( 'SHORTDESC', string.format( 
-		inlineIf( tonumber( planets ) == 1, t( 'shortdesc_singular' ), t( 'shortdesc_plural' ) ), 
-		string.gsub( string.lower( translatedType ), '^%l', string.upper ),
-		planets
+		inlineIf( mega[ 'planet_count' ] == 1, t( 'shortdesc_singular' ), t( 'shortdesc_plural' ) ), 
+		string.gsub( string.lower( mega[ '#type' ] ), '^%l', string.upper ),
+		mega[ 'planet_count' ]
 	) )
 
-	local categories = { 'Systems' }
-	if affiliation then table.insert( categories, affiliation .. ' Systems' ) end
-
-	return tostring( infobox:renderInfobox( nil, name ) ) .. convertCategories( categories )
+	return tostring( infobox:renderInfobox( nil, mega[ '#name' ] ) ) .. convertCategories( mega[ 'categories' ] )
 end
 
 function System.test( name )
-	if not name then name = 'Stanton' end
-	
-	local systemObjects = Starmap.systemObjects( name )
-	local planets = filter( systemObjects, 'type', 'PLANET' )
-	local stars = filter( systemObjects, 'type', 'STAR' )
+	if not name then name = 'Stanton' end -- System and Star
 end
 
 return System
